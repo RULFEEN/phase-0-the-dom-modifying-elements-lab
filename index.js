c3a2b6f7b4d497f522f060f6ae7c10d7d8e3e21c
Victor Mayi <victormay.i.797@gmail.com>
2022-08-14 16:25:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const body = document.getElementsByTagName("body");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.className = "victory";
+
+newHeader.textContent = "RULFEEN is the champion!";
